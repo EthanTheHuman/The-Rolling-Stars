96ca74426dad0af09f08572461d66c2679fc344e
Brandon <brandonamo3c@gmail.com>
2018-10-29 16:42:56 +1300

    j

--- The-Rolling-Stars/my first opengl/CubeMap.cpp
+++ The-Rolling-Stars/my first opengl/CubeMap.cpp
@@ -98,6 +98,7 @@ CubeMap::CubeMap(Camera * _Camera, GLuint _Program, char * _Top, char * _Bottom,
 			image = SOIL_load_image(fullPathName.c_str(), &width, &height, 0, SOIL_LOAD_RGB);
 			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
 		}
+		textureFilePaths.clear();
 
 		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
